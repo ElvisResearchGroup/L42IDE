e388e313d855a32027e49715e75c510de8a538d8
Zach <Zach@10.140.80.247>
2022-01-31 13:27:39 +1300

    fixed

--- src/is/L42/repl/js/theme-l42_eclipse.js
+++ src/is/L42/repl/js/theme-l42_eclipse.js
@@ -3,22 +3,22 @@ define("ace/theme/l42_eclipse",["require","exports","module","ace/lib/dom"], fun
 exports.isDark = false;
 exports.cssClass = "ace-l42-eclipse";
 exports.cssText = ".ace-l42-eclipse .ace_gutter {\
-background:"+ Styling.GUTTER_BACKGROUND + ";\
+background: " + Styling.GUTTER_BACKGROUND + ";\
 border-right: 1px solid rgb(159, 159, 159);\
-color: "+ Styling.GUTTER_COLOR +";\
+color: " + Styling.GUTTER_COLOR + ";\
 }\
 .ace-l42-eclipse .ace_fold {\
-background-color: "+ Styling.FOLD_BACKGROUND +";\
+background-color: " + Styling.FOLD_BACKGROUND + ";\
 }\
 .ace-l42-eclipse .ace_cursor {\
-color: "+ Styling.CURSOR_COLOR +";\
+color: " + Styling.CURSOR_COLOR + ";\
 }\
 .ace-l42-eclipse .ace_keyword{\
-color: "+ Styling.KEYWORD_COLOR +";\
+color: " + Styling.KEYWORD_COLOR + ";\
 }\
 .ace-l42-eclipse .ace_errorHighlight {\
-color: "+ Styling.ERROR_COLOR +";\
-background-color: "+ Styling.ERROR_HIGHLIGHT+";\
+color: " + Styling.ERROR_COLOR + ";\
+background-color: " + Styling.ERROR_HIGHLIGHT + ";\
 font-weight: bold\
 }\
 .ace-l42-eclipse .ace_reuselibrary {\
@@ -26,36 +26,36 @@ font-weight: bold;\
 font-style: italic;\
 }\
 .ace-l42-eclipse .ace_string {\
-color: "+ Styling.STRING_COLOR +";\
+color: " + Styling.STRING_COLOR + ";\
 }\
 .ace-l42-eclipse .ace_comment {\
-color: "+ Styling.COMMENT_COLOR +";\
+color: " + Styling.COMMENT_COLOR + ";\
 }\
 .ace-l42-eclipse .ace_marker-layer .ace_selection {\
-background: "+ Styling.SELECTION_HIGHLIGHT +";\
+background: " + Styling.SELECTION_HIGHLIGHT + ";\
 }\
 .ace-l42-eclipse .ace_marker-layer .ace_bracket {\
 margin: -1px 0 0 -1px;\
-border: 1px solid "+ Styling.BRACKET_BORDER +";\
+border: 1px solid " + Styling.BRACKET_BORDER + ";\
 }\
 .ace-l42-eclipse .ace_active-line {\
-background: "+ Styling.ACTIVE_LINE_HIGHLIGHT +";\
+background: " + Styling.ACTIVE_LINE_HIGHLIGHT + ";\
 }\
 .ace-l42-eclipse .ace_gutter-active-line {\
-background-color : "+ Styling.ACTIVE_LINE_GUTTER_HIGHLIGHT +";\
+background-color : " + Styling.ACTIVE_LINE_GUTTER_HIGHLIGHT + ";\
 }\
 .ace-l42-eclipse .ace_marker-layer .ace_selected-word {\
-border: 1px solid rgb(181, 213, 255);\
+border: 1px solid " + Styling.SELECTED_BORDER + ";\
 }\
 .ace-l42-eclipse .ace_upperIdentifiers {\
 font-weight: bold\
 }\
 .ace-l42-eclipse .ace_methodParameters {\
-color: "+ Styling.PARAMETER_COLOR +";\
+color: " + Styling.PARAMETER_COLOR + ";\
 font-style: italic;\
 }\
 .ace-l42-eclipse .ace_objectCall {\
-color: "+ Styling.OBJECT_COLOR +";\
+color: " + Styling.OBJECT_COLOR + ";\
 font-style: italic;\
 }";
 
